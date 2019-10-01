349827dd4ec85335f6812d2c3a6577383a272b0f
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-10-01 09:56:48 -0400

    Added directory taxonomy class shell. Stashing.

---
+++ includes/directory-post-type.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Code related to the directory
+ * custom taxonomy
+ */
+
+namespace UCFDocument\Taxonomies;
+
+class Directory {
+	public static function register() {
+
+	}
+
+	public static function labels( $labels ) {
+
+	}
+
+	public static function args( $labels ) {
+
+	}
+}
