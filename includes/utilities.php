b1252634248cd2ec27b1252797e82be08b0e48e4
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-10-01 09:49:20 -0400

    Replaced copied over code

--- includes/utilities.php
+++ includes/utilities.php
@@ -18,7 +18,7 @@ function acf_is_active( $required_version = '5.0.0' ) {
 	$plugin_path   = ABSPATH . 'wp-content/plugins/';
 
 	// See if the pro version is installed
-	if ( is_plugin_active( self::$acf_pro_file_location ) ) {
+	if ( is_plugin_active( $acf_pro_path ) ) {
 		$plugin_data = get_plugin_data( $plugin_path . $acf_pro_path );
 
 		if ( is_above_version( $plugin_data['Version'], $required_version ) ) {
