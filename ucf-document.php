f1fd6c71b42c13b99c189d9cd408237154d71bbd
Jo Dickson <jogreybill@gmail.com>
2021-05-07 10:11:53 -0400

    Updated version number and changelog

--- ucf-document.php
+++ ucf-document.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Document Plugin
 Description: Provides a custom post type and taxonomies for managing documents
-Version: 0.2.3
+Version: 0.2.4
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/UCF-Document-Plugin
